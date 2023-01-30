4f7cb01e55e7efbf08b39e21d6ca9ab6344bfbc8
abdulla <abdullaabdullaev0803@gmail.com>
2023-01-30 07:15:02 +0300

    add necessary loops for repetiton of the fuctions

--- scissors.js
+++ scissors.js
@@ -3,38 +3,37 @@ let computerChoice = function(){
     const choices = ["rock", "paper", "scissors"]
     return choices [Math.floor(Math.random()*3)] 
 }
-// here we made big mistake creating new function 
+let computer = 1
+let player = 1
 // this function takes 2 parameters and compares them as a result logging the winner 
     function playRound (choice1, choice2)
     {  
     if (choice1 === choice2 ){
-        console.log("no winner today1") 
+        console.log("no winner today1", "both of you",player++, computer++ ) 
     } 
 
     if ( choice2 === "scissors"){
-        console.log("comp lost 2" )
+        console.log("comp lost 2 player now has", player++)
     }
     if (choice2 ==="paper") {
-        console.log("comp wins3"  )
+        console.log("comp wins3","computer has", computer++)
         }
     }   
 const choice1 = "rock";
-const choice2 = computerChoice () ;
 // this function contains another function and random number generator 
 function game (){
-return playRound (choice1,computerChoice()) 
+playRound (choice1,computerChoice()) 
+return 
 }
 // this loop calls the function 5 times and 
-for (let i = 0; i < 10; i++) {  
+for (let i = 0; i < 3; i++) {  
     game () 
 }
 // this variable shows computerChoice function, its made so because we do not want the function to be called again 
 
-console.log (choice2)
 
 
-
-
+    
 
 
 // const playerSelection = prompt ("Hi ! Choose your weapon ?").toLowerCase();    
